ef46c4c3fe1e6ce3cb32a36dc5bc99604cd02ef0
maui <maui@devf.mx>
2019-06-16 15:22:45 -0600

    Agregar formulario y logica de este con state

--- src/components/Clima/Clima.js
+++ src/components/Clima/Clima.js
@@ -2,7 +2,28 @@ import React, { Component } from 'react';
 import {Button, Input, Label, Form, FormGroup} from 'reactstrap';
 
 class Clima extends Component {
-    state = {  }
+    state = { 
+        ciudad: '',
+        fecha: null,
+    }
+
+    manejarInputs = (e) => {
+        // console.log(e.target.name, e.target.value);
+        switch (e.target.name) {
+            case 'ciudad':
+                this.setState({ciudad: e.target.value})
+                break;
+            case 'fecha':
+                this.setState({fecha: e.target.value})
+                break;
+            default:
+                break;
+        };
+    };
+
+    enviarDatos = () => {
+        console.log(this.state);
+    };
 
     render() { 
         return ( 
@@ -10,18 +31,23 @@ class Clima extends Component {
                 <FormGroup>
                     <Label>Ciudad</Label>
                     <Input 
+                    onChange={ (e) => this.manejarInputs(e) }
                     className="mb-3"
                     type="text"
                     name="ciudad"
                     id="id_ciudad"
                     placeholder="Introduce la ciudad a buscar"/>
                     <Label>Fecha</Label>
-                    <Input 
+
+                    <Input
+                    onChange={ (e) => this.manejarInputs(e) } 
                     type="datetime"
                     name="fecha"
                     id="id_fecha"
                     placeholder="Introduce la fecha mm/dd/yyyy"/>
+                    
                     <Button 
+                        onClick={this.enviarDatos}
                         className="mt-3"
                         color="info">
                             Enviar
