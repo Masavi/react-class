23616a9b284733de785e12162e1c72d20aafbf4a
maui <maui@devf.mx>
2019-06-16 13:55:31 -0600

    agregar logica del contador y renderizado de estilos segun el valor

--- src/components/Contador/Contador.js
+++ src/components/Contador/Contador.js
@@ -2,24 +2,72 @@ import React, { Component } from 'react';
 import {Badge, Container, Button} from 'reactstrap';
 
 class Contador extends Component {
-    state = {  }
+    state = { 
+        titulo: "Alpha",
+        valor: 7,
+    };
+
+    incrementarContador = () => {
+        this.setState({
+            valor: this.state.valor + 1
+        });
+    };
+
+    decrementarContador = () => {
+        this.setState({
+            valor: this.state.valor - 1
+        });
+    };
+
+    cambiarEstilos = () => {
+        if (this.state.valor === 0) return this.setState({valor: 'cero'});
+    };
+
+    mostrarContador = () => {
+        if (this.state.valor === 0) {
+            return  <Badge color="warning">
+                        <span style={{fontSize:20}}>
+                            cero
+                        </span>
+                    </Badge>;
+        } else if (this.state.valor > 0) {
+            return  <Badge color="success">
+                        <span style={{fontSize:20}}>
+                            {this.state.valor}
+                        </span>
+                    </Badge>;
+        } else if (this.state.valor < 0) {
+            return  <Badge color="danger">
+                        <span style={{fontSize:20}}>
+                            {this.state.valor}
+                        </span>
+                    </Badge>;
+        }
+    };
+
     render() { 
         return ( 
             <Container style={{
-                width: "300px",
-                border: "2px solid black"
+                width: "350px",
+                border: "2px solid black",
+                borderRadius: '5px',
             }}>
-                <h2 className="ml-2">Hola</h2>
-                <Button className="m-2" color="primary">+</Button>
-                <Badge>
-                    <span
-                    style={{
-                        fontSize: 20
-                    }}>
-                        5
-                    </span>
-                </Badge>
-                <Button className="m-2" color="danger">-</Button>
+                <h2 className="ml-2">
+                    {this.state.titulo}
+                </h2>
+                <Button 
+                    onClick={this.incrementarContador}
+                    className="m-2" 
+                    color="primary">
+                        +
+                </Button>
+                { this.mostrarContador() }
+                <Button 
+                    onClick={this.decrementarContador}
+                    className="m-2" 
+                    color="danger">
+                        -
+                </Button>
                 <Button className="ml-5" color="warning">Eliminar</Button>
             </Container>
         );
