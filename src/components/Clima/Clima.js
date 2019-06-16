35c8a740c37f43659ee5b16eb75ba7d48dc37099
maui <maui@devf.mx>
2019-06-16 15:49:45 -0600

    agregar peticion con axios a api de clima y mostrar en consola

--- src/components/Clima/Clima.js
+++ src/components/Clima/Clima.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import {Button, Input, Label, Form, FormGroup} from 'reactstrap';
+import axios from 'axios';
 
 class Clima extends Component {
     state = { 
         ciudad: '',
         fecha: null,
-    }
+        respuesta: 'aun no hay respuesta',
+    };
 
     manejarInputs = (e) => {
         // console.log(e.target.name, e.target.value);
@@ -21,39 +23,61 @@ class Clima extends Component {
         };
     };
 
+    getCiudad = async (ciudad) => {
+        const uri = `https://www.metaweather.com/api/location/search/?query=${ciudad}`
+        try {
+          const response = await axios.get(uri);
+          const res_id = response.data.woeid;
+          const sendDate = this.state.fecha;
+          const final_uri = `https://www.metaweather.com/api/location/${res_id}/${sendDate}/`;
+          const final_response = await axios.get(final_uri);
+          console.log(final_response);
+          if (final_response.status === 200) {
+            this.setState({respuesta: final_response.status});
+          }
+        } catch (error) {
+          console.error(error);
+        }
+      }
+
     enviarDatos = () => {
-        console.log(this.state);
-    };
+        this.getCiudad(this.state.ciudad);
+    }
 
     render() { 
-        return ( 
-            <Form>
-                <FormGroup>
-                    <Label>Ciudad</Label>
-                    <Input 
-                    onChange={ (e) => this.manejarInputs(e) }
-                    className="mb-3"
-                    type="text"
-                    name="ciudad"
-                    id="id_ciudad"
-                    placeholder="Introduce la ciudad a buscar"/>
-                    <Label>Fecha</Label>
+        return (
+            <React.Fragment>      
+                <Form>
+                    <FormGroup>
+                        <Label>Ciudad</Label>
+                        <Input 
+                        onChange={ (e) => this.manejarInputs(e) }
+                        className="mb-3"
+                        type="text"
+                        name="ciudad"
+                        id="id_ciudad"
+                        placeholder="Introduce la ciudad a buscar"/>
+                        <Label>Fecha</Label>
 
-                    <Input
-                    onChange={ (e) => this.manejarInputs(e) } 
-                    type="datetime"
-                    name="fecha"
-                    id="id_fecha"
-                    placeholder="Introduce la fecha mm/dd/yyyy"/>
-                    
-                    <Button 
-                        onClick={this.enviarDatos}
-                        className="mt-3"
-                        color="info">
-                            Enviar
-                    </Button>
-                </FormGroup>
-            </Form>
+                        <Input
+                        onChange={ (e) => this.manejarInputs(e) } 
+                        type="datetime"
+                        name="fecha"
+                        id="id_fecha"
+                        placeholder="Introduce la fecha mm/dd/yyyy"/>
+                        
+                        <Button 
+                            onClick={this.enviarDatos}
+                            className="mt-3"
+                            color="info">
+                                Enviar
+                        </Button>
+                    </FormGroup>
+                </Form>
+                <p><b>
+                    { this.state.respuesta }
+                </b></p>
+            </React.Fragment> 
         );
     }
 }
