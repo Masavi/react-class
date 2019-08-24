043d9a9090958e94dee8a7768297dd5d29383714
maui <maui@devf.mx>
2019-08-24 11:08:34 -0600

    agregar logica de incrementar y decrementar contador con setState y arrow functions

--- src/components/Contador.js
+++ src/components/Contador.js
@@ -7,16 +7,31 @@ class Contador extends Component {
         valor: 0,
     }
 
+    incrementar = () => {
+        this.setState({
+            valor: this.state.valor + 1,
+        });
+    }
+
+    decrementar = () => {
+        this.setState({
+            valor: this.state.valor - 1,
+        });
+    }
+
     render() { 
         return ( 
             <React.Fragment>
                 <h2 className='mt-4'>{this.state.titulo}</h2>
                 <div className='d-flex align-items-center'>
                     <Badge style={{fontSize: '1.55em'}}>{this.state.valor}</Badge>
-                    <Button 
+                    <Button
+                        onClick={this.incrementar}
                         className='ml-2 mr-2'
                         color='primary'>+</Button>
-                    <Button color='danger'>-</Button>
+                    <Button 
+                        onClick={this.decrementar}
+                        color='danger'>-</Button>
                 </div>
             </React.Fragment>
         );
