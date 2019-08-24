5c2e3bf9cf35fb9ebcdf0112ce973902451c0870
maui <maui@devf.mx>
2019-08-24 11:13:20 -0600

    renderizar estilos del valor del contador dinamicamente

--- src/components/Contador.js
+++ src/components/Contador.js
@@ -19,12 +19,28 @@ class Contador extends Component {
         });
     }
 
+    renderValor = () => {
+        if (this.state.valor > 0) {
+            return <Badge
+                color='primary'
+                style={{fontSize: '1.55em'}}>{this.state.valor}</Badge>
+        } else if (this.state.valor < 0) {
+            return <Badge 
+                color='danger'
+                style={{fontSize: '1.55em'}}>{this.state.valor}</Badge>
+        } else {
+            return <Badge 
+                color='warning'
+                style={{fontSize: '1.55em'}}>cero</Badge>
+        }
+    }
+
     render() { 
         return ( 
             <React.Fragment>
                 <h2 className='mt-4'>{this.state.titulo}</h2>
                 <div className='d-flex align-items-center'>
-                    <Badge style={{fontSize: '1.55em'}}>{this.state.valor}</Badge>
+                    { this.renderValor() }
                     <Button
                         onClick={this.incrementar}
                         className='ml-2 mr-2'
