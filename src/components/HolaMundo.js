2420f23748184f85c27dff80a03e3ae018edfdff
maui <maui@devf.mx>
2019-08-24 10:33:52 -0600

    instalar y probar bootstrap

--- src/components/HolaMundo.js
+++ src/components/HolaMundo.js
@@ -5,14 +5,14 @@ class HolaMundo extends Component {
 
     render() { 
         return ( 
-            <React.Fragment>
+            <div className='container'>
                 <h1 
                 className='miclase'
                 style={{color: 'green',fontSize:'100px'}}>
                     Hola, soy {this.nombre}
                 </h1>
                 <img alt='perro salchicha' src='https://www.purina.es/sites/g/files/mcldtz1656/files/2019-06/Perro%20salchicha%20%28Miniatura%20de%20pelo%20liso%29_400x378_0.jpg'/>
-            </React.Fragment>
+            </div>
         );
     }
 }
