2668c9f153abbef87558e1be8ce7d47cb9d31ac9
maui <maui@devf.mx>
2019-10-10 21:46:31 -0500

    agregar bootstrap

--- src/components/HolaMundo.js
+++ src/components/HolaMundo.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button
+  } from 'reactstrap';
 
 class HolaMundo extends Component {
     state = { 
@@ -14,6 +18,18 @@ class HolaMundo extends Component {
                 <h1>{this.state.nombre}</h1>
                 <h2>{this.state.ocupacion}</h2>
                 <h3>{this.state.pasatiempos}</h3>
+                <div>
+                    <Card>
+                        <CardImg style={{height: '100px', width: '100px'}}
+                        top width="100%" src="https://www.purina.es/sites/g/files/mcldtz1656/files/2019-06/Perro%20salchicha%20%28Miniatura%20de%20pelo%20liso%29_400x378_0.jpg" alt="Card image cap" />
+                        <CardBody>
+                        <CardTitle>Card title</CardTitle>
+                        <CardSubtitle>Card subtitle</CardSubtitle>
+                        <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                        <Button>Button</Button>
+                        </CardBody>
+                    </Card>
+                </div>
             </React.Fragment>
         );
     }
