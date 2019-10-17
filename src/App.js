d8bd98815375ffd19c839d3c1e1ecc014472e7e8
maui <maui@devf.mx>
2019-10-16 21:12:57 -0500

    agregar rutas con react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import './App.css';
 import Articulos from './components/Articulos';
-// import Contadores from './components/Contadores';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <React.Fragment>
-      <Articulos />
-    </React.Fragment>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/articulos">About</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/articulos">
+              <Articulos />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/CrearArticulo.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Container } from 'reactstrap';
+
+class CrearArticulo extends Component {
+    state = {  }
+
+    render() { 
+        return ( 
+            <Container className='mt-4'>
+                <h1>Crear Artículo</h1>
+            </Container>
+        );
+    }
+}
+ 
+export default CrearArticulo;
